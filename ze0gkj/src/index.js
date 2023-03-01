f757dac6ef130cb663bffc8083528ad156dcc714
jin mo byeon <107828109+byeonjinmo@users.noreply.github.com>
2023-03-02 01:16:34 +0900

    Add files via upload
    String(숫자값)을 이용하여 숫자값을 문자열로 바꾸고 pad.Start(2,"0"); 을 이용하여 시계 출력시 한자리 수가 01, 02 로 출력 가능하게 함

--- ze0gkj/src/index.js
+++ ze0gkj/src/index.js
@@ -6,12 +6,19 @@ const clock = setInterval(function () {
   const distance = xmas - now;
 
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  const hours = String(
+    Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   );
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  clockTitle.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  const minutes = String(
+    Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  );
+  const seconds = String(Math.floor((distance % (1000 * 60)) / 1000));
+  const seconds1 = seconds.padStart(2, "0");
+  const minutes1 = minutes.padStart(2, "0");
+  const hours1 = hours.padStart(2, "0");
+
+  String(new Date().getHours()).padStart(2, "0");
+  clockTitle.innerText = `${days}d ${hours1}h ${minutes1}m ${seconds1}s`;
   if (distance < 0) {
     clearInterval(clock);
     clockTitle.innerText = "IT`S D-DAY!!";
